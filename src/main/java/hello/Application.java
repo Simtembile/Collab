957ce2676005eb9e4a417430307ec9381f1210f3
Soginga Simtembile <Soginga Simtembile@Simtembile>
2018-02-14 10:00:06 +0300

    fixing external files issue? #1

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -2,6 +2,7 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
+@SpringBootApplication
 
 public class Application extends SpringBootServletInitializer {
 
